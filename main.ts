157268d81cf6e78639ce7e48dfda071d3c00aca7
VicRoger27 <121492850+VicRoger27@users.noreply.github.com>
2023-01-30 11:51:41 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    _2o2 = 2
+})
+input.onButtonPressed(Button.A, function () {
+    if (1 > _2o2) {
+        game.gameOver()
+        music.playTone(262, music.beat(BeatFraction.Breve))
+    } else {
+        basic.pause(500)
+        game.addScore(1)
+        basic.pause(500)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    game.gameOver()
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    _2o2 = 0
+})
+let _2o2 = 0
+_2o2 = 2
+basic.forever(function () {
+    basic.pause(500)
+    game.addScore(1)
+    basic.pause(500)
+})
